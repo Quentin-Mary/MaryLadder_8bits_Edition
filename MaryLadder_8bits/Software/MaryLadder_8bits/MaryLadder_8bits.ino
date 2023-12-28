89aa38c94a461f8a52a12790ef70a45706596cc3
Quentin Mary <quentin.mary@reseau.eseo.fr>
2023-12-28 23:09:22 +0100

    Update MaryLadder_8bits.ino

--- MaryLadder_8bits/Software/MaryLadder_8bits/MaryLadder_8bits.ino
+++ MaryLadder_8bits/Software/MaryLadder_8bits/MaryLadder_8bits.ino
@@ -45,33 +45,13 @@ bool read_mux_channel(int channel){
   return val_bit;
 }
 
-// void read_mux(bool* tab_value){
-//   bool value;
-//   //bool tab_value[8];
-//   for(int i=0; i<8; i++){
-//     value = read_mux_channel(i);
-//     tab_value[i] = value;
-//   }
-// }
-
-// int bits_weight(bool tab_value[]){
-//   int sum = 0;
-//   int table_weight[8] = {128,64,32,16,8,4,2,1};
-//   //int table_weight[8] = {1,2,4,8,16,32,64,128};
-
-//   for(int i=0; i<8; i++){
-//     if(tab_value[i] == true){
-//       sum = sum + table_weight[i];
-//     }
-//   }
-
-//   return sum;
-// }
+unsigned long currentMillis = millis();
+const unsigned long interval_disp = 5;
+unsigned long previousMillis_disp = 0;
 
 int test_read_bits_weight(){
   bool value;
   int sum = 0;
-  //int table_weight[8] = {128,64,32,16,8,4,2,1};
   int table_weight[8] = {1,2,4,8,16,32,64,128};
 
   for(int i=0; i<8; i++){
@@ -115,28 +95,37 @@ void write_disp_complete(int num){
   turnOFF_Digit(DQ_2);
   turnOFF_Digit(DQ_3);
   write_disp(digit_1);
-  delay(5);
+  delay(5);  
+  while (millis() - previousMillis_disp < interval_disp) {
+  }
+  previousMillis_disp = millis();
+
 
   turnOFF_Digit(DQ_1);
   turnON_Digit(DQ_2);
   turnOFF_Digit(DQ_3);
   write_disp(digit_2);
-  delay(5);
+  //delay(5);
+  while (millis() - previousMillis_disp < interval_disp) {
+  }
+  previousMillis_disp = millis();
 
   turnOFF_Digit(DQ_1);
   turnOFF_Digit(DQ_2);
   turnON_Digit(DQ_3);
   write_disp(digit_3);
-  delay(5);
+  //delay(5);
+  while (millis() - previousMillis_disp < interval_disp) {
+  }
 }
 
 
 void disp_test(){
-  for(int i=0; i<10; i++){
-    write_disp(i);
-    delay(500);
+  for(int i=0; i<255; i++){
+    write_disp_complete(i);
+    delay(100);
   }
-  delay(3000);
+  delay(1000);
 }
 
 void turnON_Digit(int digit){
@@ -145,6 +134,18 @@ void turnON_Digit(int digit){
 
 void turnOFF_Digit(int digit){
   digitalWrite(digit, LOW);
+}
+
+int check_bit(int bit_num){
+  int value_bit;
+  bool bool_val = read_mux_channel(bit_num);
+  if(bool_val == true){
+    value_bit = 1;
+  }
+  else{
+    value_bit = 0;
+  }
+  return value_bit;
 }
 //---//
 
@@ -165,156 +166,15 @@ void setup() {
   //Outputs to select each bit on the mux
   pinMode(M_A, OUTPUT);
   pinMode(M_B, OUTPUT);
-  pinMode(M_B, OUTPUT);
+  pinMode(M_C, OUTPUT);
 
   //Input to get the bit selected on the mux
   pinMode(M_Y, INPUT); 
-
-
 }
 
-
-typedef enum{
-  INIT,
-  BIT_0,
-  BIT_1,
-  BIT_2,
-  BIT_3,
-  BIT_4,
-  BIT_5,
-  BIT_6,
-  BIT_7,
-  DISPLAY_SUM
-}Mode_e;
-
-static Mode_e mode = INIT;
-
-int check_bit(int bit_num){
-  int value_bit;
-  bool bool_val = read_mux_channel(bit_num);
-  if(bool_val == true){
-    value_bit = 1;
-  }
-  else{
-    value_bit = 0;
-  }
-  return value_bit;
-}
 
 void loop() {
-
-  //disp_test();
-  //write_disp_complete(123);
-
-  // bool val = read_mux_channel(7);
-  // if(val == true){
-  //   write_disp_complete(1);
-  // }
-  // else{
-  //   write_disp_complete(0);
-  // }
-
-  // sum = test_read_bits_weight();
-  // write_disp_complete(sum);
-  // sum = 0;
-
-  switch (mode){
-    case INIT:
-      int sum = 0;
-      int check_bit_result = 0;
-
-      // write_disp_complete(1);
-      // delay(100);
-      // write_disp_complete(0);
-
-      mode = DISPLAY_SUM;
-      break;
-    
-    case BIT_0:
-      check_bit_result = check_bit(0);
-      if(check_bit_result == 1){
-        sum = 1;
-      }
-      check_bit_result = 0;
-      delay(200);
-      mode = BIT_1;
-      break;
-
-    case BIT_1:
-      check_bit_result = check_bit(1);
-      if(check_bit_result == 1){
-        sum = sum + 2;
-      }
-      check_bit_result = 0;
-      delay(200);
-      mode = BIT_2;
-      break;
-
-
-    case BIT_2:
-      check_bit_result = check_bit(2);
-      if(check_bit_result == 1){
-        sum = sum + 4;
-      }
-      check_bit_result = 0;
-      mode = BIT_3;
-      break;
-
-    case BIT_3:
-      check_bit_result = check_bit(3);
-      if(check_bit_result == 1){
-        sum = sum + 8;
-      }
-      check_bit_result = 0;
-      mode = BIT_4;
-      break;
-
-    case BIT_4:
-      check_bit_result = check_bit(4);
-      if(check_bit_result == 1){
-        sum = sum + 16;
-      }
-      check_bit_result = 0;
-      mode = BIT_5;
-      break;
-
-    case BIT_5:
-      check_bit_result = check_bit(5);
-      if(check_bit_result == 1){
-        sum = sum + 32;
-      }
-      check_bit_result = 0;
-      mode = BIT_6;
-      break;
-
-    case BIT_6:
-      check_bit_result = check_bit(6);
-      if(check_bit_result == 1){
-        sum = sum + 64;
-      }
-      check_bit_result = 0;
-      mode = BIT_7;
-      break;
-
-    case BIT_7:
-      check_bit_result = check_bit(7);
-      if(check_bit_result == 1){
-        sum = sum + 128;
-      }
-      check_bit_result = 0;
-      mode = DISPLAY_SUM;
-      break;  
-
-    case DISPLAY_SUM:
-      //write_disp_complete(124);
-      sum = 0;
-      mode = INIT;
-      break; 
-
-    default:
-      break; 
-  }
-
-  
-
+  int disp_sum = 0;
+  disp_sum = test_read_bits_weight();
+  write_disp_complete(disp_sum);
 }
